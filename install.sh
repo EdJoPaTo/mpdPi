e0a730c2938a5798cf30b1b1855b9963abf945b2
Edgar Toll <edjopato@gmail.com>
2018-07-06 11:07:14 +0200

    feat: do less important things silent

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ sudo apt install mpd mpc
 sudo cp playlists/* /var/lib/mpd/playlists/
 sudo chown -R mpd:audio /var/lib/mpd/
 
-mpc update --wait
-mpc load Chillout
+mpc -q update --wait
+mpc -q load Chillout
 mpc play
