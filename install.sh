90dd10e46ea4943d0b7b05e286e61fdee72068a4
Edgar Toll <edjopato@gmail.com>
2018-07-07 10:17:15 +0200

    feat: add systemd timer to restart playback after network reconnect

--- install.sh
+++ install.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
 
+# install mpd
 sudo apt install mpd mpc
 
+# copy playlists
 sudo cp playlists/* /var/lib/mpd/playlists/
 sudo chown -R mpd:audio /var/lib/mpd/
 
+# copy systemd units
+sudo cp -uv systemd/* /etc/systemd/system
+
+# reload systemd
+sudo systemctl daemon-reload
+
+# start automatic restart timer
+sudo systemctl enable mpd-restart.timer
+sudo systemctl start mpd-restart.timer
+
+# start default stream
 mpc -q update --wait
 mpc -q load Chillout
 mpc -q volume 100
