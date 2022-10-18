d919d35a538bc905bc8321e290bcdc752da1e1e4
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2022-10-18 15:10:50 +0200

    fix(systemd): prepare and dont automatically play

--- scripts/mpd-restart.sh
+++ scripts/mpd-restart.sh
@@ -3,6 +3,6 @@ set -eu
 
 /usr/bin/mpc -q clear
 /usr/bin/mpc -q update --wait
-/usr/bin/mpc -q volume 15
+/usr/bin/mpc -q volume 20
 /usr/bin/mpc -q load Chillout
-/usr/bin/mpc play 1
+#/usr/bin/mpc play 1
